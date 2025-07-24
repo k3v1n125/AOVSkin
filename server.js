89d4910670bb410d6fcf988b75e8bb34b2b19402
Kevin Wang <z5553611@ad.unsw.edu.au>
2025-07-24 11:44:24 +1000

    test

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.get("/data", (req, res) => {
   res.json(data);
 });
 
+//Test
 // PUT: Edit an existing entry by ID
 app.put("/edit/:id", (req, res) => {
   const id = parseInt(req.params.id);
